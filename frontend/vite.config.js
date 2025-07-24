ccd6bda621801761b1a195466fdfc2f0eb98f8d0
Jhoan Quintero <jhoanfnaf@gmail.com>
2025-07-24 18:43:13 -0500

    Rutas ajustadas para netlify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: '../netlify-build',
+    outDir: './netlify-build',
     emptyOutDir: true,
     rollupOptions: {
       output: {
